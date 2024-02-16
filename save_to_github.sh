6d5fa1c85eb08e55c5157c663d28b44fd68516c6
idajohannesen <idabj@hotmail.dk>
2024-02-16 15:13:06 +0100

    Uploading add, commit, push script

---
+++ save_to_github.sh
@@ -0,0 +1,8 @@
+# navigate to correct folder
+# edit filenames
+git config --global user.name "idajohannesen"
+git config --global user.email "idabj@hotmail.dk"
+git add save_to_github.sh
+git commit -m "Uploading add, commit, push script"
+git push origin main
+# write bash save_to_github.sh in terminal
